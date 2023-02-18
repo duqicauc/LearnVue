8747df350277d1d1483adea40a35e5c016fc9897
duqicauc <nkduqi@126.com>
2023-02-18 16:30:28 +0800

    first commit

---
+++ vite-project/src/components/test1.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>{{state.name}}</div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+export default {
+    setup() {
+        const state = reactive({ name: '阿杜'})
+        return {state}
+    }
+}
+</script>
